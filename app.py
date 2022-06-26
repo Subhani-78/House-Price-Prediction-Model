f02d4c52b66c1a3ceefe9a99af3b8023da7da027
Mujeeb <mujeeb.subhani78@gmail.com>
2022-06-27 02:49:14 +0500

    Logging has been setup

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from housing.logger import logging
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def hello_world():
+    logging.info("Testing Logging Module")
     return 'This app has been deployed on the cloud.'
 
 
