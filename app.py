3575de858640713e8936c87eaff070ee582f6e01
Mujeeb <mujeeb.subhani78@gmail.com>
2022-06-27 05:57:06 +0500

    Exception Code Added

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import CodeException
+
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def hello_world():
-    logging.info("Testing Logging Module")
+    try:
+        raise Exception("We are testing custom exception.")
+    except Exception as e:
+        housing = CodeException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("Testing Logging Module")
+
     return 'This app has been deployed on the cloud.'
 
 
